7777c3f9522a3e1635828ca446ef1d986a91a60e
alicia-breinke <leeshia12@gmail.com>
2020-07-28 13:29:13 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 let img;
-let r;
-let g;
-let b;
-let y;
+var r = 255;
+var g = 64;
+var b = 0;
+let d;
+let a;
+let s;
 let c;
 var increase = 5;
 var decrease = 5;
@@ -13,7 +15,7 @@ function setup(){
   c = createCanvas(975, 506);
   background(100, 100, 100);
   image(img, 50, 50);
-  stroke('rgb(255, 255, 0)');
+  stroke ('rgb(255, 255, 0)');
 }
 
 function draw () {
@@ -26,21 +28,30 @@ function draw () {
 
 function keyPressed() {
   if (key === 'r') {
-    stroke(255, 0 , 0);
-    }
+    r = 255;
+    g = 114;
+    b = 0;
+    print(r);
+  }
   if (key === 'g') {
-    stroke(0, 64, 0);
+    r = 0;
+    g = 114;
+    b = 0;
   }
   if (key === 'b') {
-    stroke(0, 180, 255);
-  }
-  if (key === 'y'){
-    stroke(255, 255, 0);
-  }
-  if (key === 'i'){
-  increase += 5;
+    r = 0;
+    g = 0;
+    b = 0;
   }
   if (key === 'd'){
+    r = 255;
+    g = 255;
+    b = 0;
+  }
+  if (key === 'a'){
+  increase += 5;
+  }
+  if (key === 's'){
     decrease += 5;
   }
   if (key === 'c'){

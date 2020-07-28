41c0c1454b0d6a94b94ff3f585f5a45b248f7921
alicia-breinke <leeshia12@gmail.com>
2020-07-28 12:50:21 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 let img;
 var increase = 5;
 var r = 244;
-var g = 64;
-var b = 0;
+var g = 255;
+var b = 255;
 let d;
 let c;
 function preload() {
@@ -26,19 +26,19 @@ function draw () {
 function keyPressed() {
   if (key === 'r') {
     r = 244;
-    g = 64;
-    b = 75;
+    g = 0;
+    b = 0;
     print(r);
   }
   if (key === 'g') {
     r = 0;
-    g = 114;
+    g = 255;
     b = 0;
   }
   if (key === 'b') {
     r = 0;
     g = 0;
-    b = 244;
+    b = 255;
   }
   if (key === 'd'){
     r = 255;

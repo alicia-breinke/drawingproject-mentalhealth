aae96e5757a2dcea9f122d3cafaf3c549cdb36b3
alicia-breinke <leeshia12@gmail.com>
2020-07-28 20:06:00 -0700

    update

--- sketch.js
+++ sketch.js
@@ -12,14 +12,13 @@ function setup(){
   c = createCanvas(975, 506);
   background(100, 100, 100);
   image(img, 50, 50);
-  stroke(r, g, b, 100);
+  stroke('rgb(244, 244, 0)');
 }
-
 function draw () {
   strokeWeight(increase);
     if (mouseIsPressed) {
   line(mouseX, mouseY, pmouseX, pmouseY);
-  stroke(244, 244, 0);
+  stroke(r, g, b, 100);
   }
 }
 

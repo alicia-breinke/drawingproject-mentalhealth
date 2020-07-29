7432ada651fa1acde6fc6e798f88edf06a6cdbc0
alicia-breinke <leeshia12@gmail.com>
2020-07-28 20:10:40 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let img;
 var increase = 5;
 var r = 100;
-var g = 100;
+var g = 64;
 var b = 0;
 let i;
 let c;
@@ -12,7 +12,7 @@ function setup(){
   c = createCanvas(975, 506);
   background(100, 100, 100);
   image(img, 50, 50);
-  stroke('rgb(100, 100, 0)');
+  stroke('rgb(64, 64, 0)');
 }
 function draw () {
   strokeWeight(increase);
@@ -30,7 +30,7 @@ function keyPressed() {
   }
   if (key === 'g') {
     r = 0;
-    g = 100;
+    g = 64;
     b = 0;
   }
   if (key === 'b') {
